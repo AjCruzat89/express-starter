a19475db91acd7afb2c1fb7d6c3b5609ee74837b
Dollr <116146758+AjCruzat89@users.noreply.github.com>
2024-09-07 23:16:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 //<!--===============================================================================================-->
 const express = require('express');
-const cors =  require('cors');
-const app = express();
+const http = require('http');
+const cors = require('cors');
 const PORT = 3000;
 //<!--===============================================================================================-->
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
