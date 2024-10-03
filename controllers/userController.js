d78ed0d80ff0d029db92b1afcfcf619f4fcfa65f
AjCruzat89 <ajcruzat89@gmail.com>
2024-10-03 21:31:21 +0800

    :D

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,9 +8,8 @@ const JWT_SECRET = 'capstone_jwt';
 const blacklist = require('./blacklist');
 //<!--===============================================================================================-->
 exports.register = async (req, res) => {
-    const { username, password } = req.body;
     try{
-        const newUser = await User.create({ username, password });
+        const newUser = await User.create(req.body);
         res.status(200).json({ message: 'User created successfully', newUser });
     }
     catch(err){
@@ -19,34 +18,27 @@ exports.register = async (req, res) => {
 }
 //<!--===============================================================================================-->
 exports.login = async (req, res) => {
-    const { username, password } = req.body;
-
     try {
         const user = await User.findOne({
              where: { 
-                username: username,
+                username: req.body.username,
                 role:{
                     [Op.or]: ['staff', 'admin']
                 } 
             } 
         });
-
         if (!user) {
-            return res.status(401).json({ error: 'Invalid username or password' });
+            return res.status(401).json({ error: 'User not found' });
         }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-
+        const isMatch = await bcrypt.compare(req.body.password, user.password);
         if (!isMatch) {
             return res.status(401).json({ error: 'Invalid username or password' });
         }
-
         const token = jwt.sign(
             { id: user.id, username: user.username, role: user.role },
             JWT_SECRET,
             { expiresIn: '24h' }    
         );
-
         res.json({ token: token, role: user.role });
     } 
     catch (err) {
@@ -61,7 +53,6 @@ exports.staffAuth = async (req, res) => {
     if (!token) {
         return res.status(401).json({ error: 'Token is required' });
     }
-
     if (blacklist.has(token)) {
         return res.status(403).json({ error: 'Token is blacklisted' });
     }
@@ -70,13 +61,10 @@ exports.staffAuth = async (req, res) => {
         if (err) {
             return res.status(403).json({ error: 'Invalid or expired token' });
         }
-
         const user = await User.findByPk(decoded.id);
-
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
-
         if (user.role !== 'staff') {
             blacklist.add(token);
             return res.status(403).json({ error: 'Unauthorized access' });
@@ -99,7 +87,6 @@ exports.adminAuth = (req, res) => {
     if (token == null) {
         return res.status(401).json({ error: 'Token is required'});
     }
-
     if (blacklist.has(token)) {
         return res.status(403).json({ error: 'Token is blacklisted'});
     }
@@ -108,18 +95,14 @@ exports.adminAuth = (req, res) => {
         if (err) {
             return res.status(403).json({ error: 'Invalid or expired token' });
         }
-
         const user = await User.findByPk(decoded.id);
-
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
-
         if (user.role !== 'admin') {
             blacklist.add(token);
             return res.status(403).json({ error: 'Unauthorized access' });
         }
-
         res.status(200).json({
             id: user.id,
             username: user.username,
@@ -137,12 +120,11 @@ exports.logout = (req, res) => {
     if (token == null) {
         return res.status(401).json({ error: 'Token is required'});
     }
-
     if(blacklist.has(token)){
         return res.status(403).json({ error: 'Token is blacklisted'});
     }
 
-    jwt.verify(token, JWT_SECRET, (err, user) => {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
         if (err) {
             return res.status(403).json({ error: 'Invalid or expired token'});
         }
@@ -156,12 +138,11 @@ exports.redirect = (req, res) => {
     const token = authHeader.split(' ')[1];
 
     if(token){
-        jwt.verify(token, JWT_SECRET, (err, user) => {
-            if (err) {
+        jwt.verify(token, JWT_SECRET, (err, decoded) => {
+            if(err){
                 return res.status(403).json({ error: 'Invalid or expired token'});
             }
-
-            res.status(200).json({ role: user.role });
+            res.status(200).json({ role: decoded.role });
         })
     }
 }
